caf376b260d07c5605db9d1f28b79630499be997
Wagner D'Amaral <32019817+wdamaral@users.noreply.github.com>
2018-05-21 00:39:57 -0400

    Issue page projects

--- server.js
+++ server.js
@@ -62,7 +62,7 @@ app.get('/about', (req, res) => {
     res.send('About page');
 });
 
-app.get('projects', (req, res) => {
+app.get('/projects', (req, res) => {
     res.render('projects.hbs', {
         pageTitle: 'Projects'
     });
