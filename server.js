e9b92a3261ee61c3200012ab05569485872f1961
Wagner D'Amaral <32019817+wdamaral@users.noreply.github.com>
2018-05-21 00:37:34 -0400

    Addi page Projects

--- server.js
+++ server.js
@@ -62,6 +62,12 @@ app.get('/about', (req, res) => {
     res.send('About page');
 });
 
+app.get('projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects'
+    });
+});
+
 app.get('/bad', (req, res) => {
     res.send({
         message: 'Challenge accepted'
